ad8aff1fe07a5b4dfba4ff174022e8d3919f34aa
Gregory Smith <greg@Gregorys-Air.local>
2019-07-06 15:08:57 -0400

    Making myserver a proxy server

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 const express = require('express');
+const https = require('https');
+
 const app = express();
 var port = process.env.PORT || 80;
 
@@ -15,9 +17,47 @@ app.use(function(req, res, next) {
 
 /* serves all the static files */
 app.get(/^(.+)$/, function(req, res) {
-    console.log('static file request : ' + req.params);
+    console.log('static file request : ');
+    console.log(req);
+    if (req.params[0].startsWith('/proxy/')) {
+        return proxy(req, res);
+    }
     res.sendFile(process.cwd() + req.params[0]);
 });
+
+//acts as a proxy server to a remote host
+function proxy(req, res) {
+    console.log('proxy request : ');
+    //console.log(req);
+    let query = '';
+    let comma = '?';
+    for (let key in req.query) {
+        let value = req.query[key];
+        query += comma + key + '=' + value;
+        comma = '&';
+    }
+
+    let url = req.params[0].substring('/proxy/'.length) + query;
+    console.log(url);
+    https
+        .get(url, resp => {
+            let data = '';
+
+            // A chunk of data has been recieved.
+            resp.on('data', chunk => {
+                data += chunk;
+            });
+
+            // The whole response has been received. Print out the result.
+            resp.on('end', () => {
+                console.log(data);
+                res.send(data);
+            });
+        })
+        .on('error', err => {
+            console.log('Error: ' + err.message);
+        });
+}
 
 app.listen(port, err => {
     if (err) {
